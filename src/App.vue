a733b7997d0c190f2411c9b297d5f32f31ea53b4
xzha0459 <xzha0459@student.monash.edu>
2025-08-24 18:38:27 +0800

    update layout

--- src/App.vue
+++ src/App.vue
@@ -3,43 +3,14 @@ import HomePage from "@/page/HomePage.vue";
 </script>
 
 <template>
-  <header>
-
-
-    <div class="wrapper">
-      <HomePage />
-    </div>
-  </header>
-
-  <main>
-  </main>
+  <div id="app">
+    <HomePage />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  width: 100%;
+  min-height: 100vh;
 }
 </style>
